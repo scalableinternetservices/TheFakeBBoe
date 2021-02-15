79d6d0d7c9cd444acd7d57c2f4fdd7a8d79f2d61
Bryan Tan <techniux@gmail.com>
2021-02-14 22:21:11 -0800

    Add meme upload logic
    - Generate meme boilerplate with scaffold
    - Modify meme image handling to use ActiveStorage
    - Implement upload form / logic

---
+++ app/controllers/memes_controller.rb
@@ -0,0 +1,74 @@
+class MemesController < ApplicationController
+  before_action :set_meme, only: %i[ show edit update destroy image ]
+
+  # GET /memes or /memes.json
+  def index
+    @memes = Meme.all
+  end
+
+  # GET /memes/1 or /memes/1.json
+  def show
+  end
+
+  # GET /memes/new
+  def new
+    @meme = Meme.new
+  end
+
+  # GET /memes/1/edit
+  def edit
+  end
+
+  # POST /memes or /memes.json
+  def create
+    @meme = Meme.new(meme_params)
+
+    respond_to do |format|
+      if @meme.save
+        format.html { redirect_to @meme, notice: "Meme was successfully created." }
+        format.json { render :show, status: :created, location: @meme }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @meme.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /memes/1 or /memes/1.json
+  def update
+    respond_to do |format|
+      if @meme.update(meme_params)
+        if params.key?(:image)
+          meme.image.attach(params[:image])
+        end
+        format.html { redirect_to @meme, notice: "Meme was successfully updated." }
+        format.json { render :show, status: :ok, location: @meme }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @meme.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /memes/1 or /memes/1.json
+  def destroy
+    @meme.destroy
+    @meme.image.purge
+    respond_to do |format|
+      format.html { redirect_to memes_url, notice: "Meme was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_meme
+      @meme = Meme.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def meme_params
+      # TODO: use current logged in user
+      params.require(:meme).permit(:title, :image, :user_id)
+    end
+end
