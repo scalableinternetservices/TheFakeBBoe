7b4e9fbb7531644536722eb68c7b4fdbbbfdf402
Bryan Tan <techniux@gmail.com>
2021-02-15 19:31:01 -0800

    Set up meme controller tests

--- test/system/memes_test.rb
+++ test/system/memes_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class MemesTest < ApplicationSystemTestCase
   setup do
-    @meme = memes(:one)
+    # @meme = memes(:one)
   end
 
   test "visiting the index" do
