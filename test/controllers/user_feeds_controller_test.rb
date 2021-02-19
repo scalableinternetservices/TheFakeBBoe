3ecf722948e9e6d16b0361903af7666f994d2199
Jiamo Liu <Jiamo.Liu95@gmail.com>
2021-02-19 18:56:41 +0200

    fixed unit tests

--- test/controllers/user_feeds_controller_test.rb
+++ test/controllers/user_feeds_controller_test.rb
@@ -1,12 +1,10 @@
 require "test_helper"
 
 class UserFeedsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user_feed = user_feeds(:one)
-  end
+
 
   test "should get user_feed" do
-    p get user_feeds_url
+    get user_feeds_url
     assert_response :success
   end
 
