d86880bebe743632e5b2ef24e43848d0843d766b
Daniel Shu <danielshu98@gmail.com>
2021-02-22 23:08:45 -0800

    Added user association to profile

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,24 +3,27 @@ class MatchesController < ApplicationController
 
     def index
         @match_profiles = []
-
+        previouslyMatchedIds = [current_user.id]
         # First, grab all matches who have liked you but you have not liked them back
-        likedMeIds = Match.where("liked == FALSE AND (user1_id == :user_id OR user2_id == :user_id", (user_id: current_user.id))
-        
-        previouslyMatchedIds = []
-        for likedMeIds do |match|
-            match_id = match.user1_id == current_user.id ?  match.user2_id : match.user1_id
-            newProfile = Profile.where(user_id: match_id).sample
-            @match_profiles.append()
-            previouslyMatchedIds.append(match_id)
+        likedMeIds = Match.where("liked = FALSE AND (user1_id = :user_id OR user2_id = :user_id)", {user_id: current_user.id})
+        if likedMeIds != nil 
+            
+            likedMeIds.each do |match|
+                match_id = match.user1_id == current_user.id ?  match.user2_id : match.user1_id
+                newProfile = Profile.where(user_id: match_id).sample
+                @match_profiles.append(newProfile)
+                previouslyMatchedIds.append(match_id)
+            end
         end
 
         # Next, grab random users until you fill up to 10 profiles or you run out
         if @match_profiles.count < 10 
-            otherProfiles = Profile.order(Arel.sql('RANDOM()')).sample(20).reject! do |p|
-                p.user_id.in?(previouslyMatchedIds)
+
+            # This is ignoring nil user_ids, which is not an allowable case but we have no idea why
+            otherProfiles = Profile.where.not(user_id: previouslyMatchedIds).sample(20)
+            if otherProfiles != nil 
+                @match_profiles = @match_profiles + otherProfiles
             end
-            @match_profiles = @match_profiles + otherProfiles
         end
 
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_052300) do
+ActiveRecord::Schema.define(version: 2021_02_23_062535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,8 +95,10 @@ ActiveRecord::Schema.define(version: 2021_02_23_052300) do
     t.bigint "rival_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["follower_id"], name: "index_profiles_on_follower_id"
     t.index ["rival_id"], name: "index_profiles_on_rival_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "user_feed_subscriptions", force: :cascade do |t|
