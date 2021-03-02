4193d85b81679f13182c2cc48b4ac51a84a6307a
Seif Ibrahim <seifibrahim10@gmail.com>
2021-03-01 21:54:51 -0800

    display memes in profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create!(username:"abc",password:"12345678", email: "abc@gmail.com")
-user2 = User.create!(username:"def",password:"12345678", email:"bcd@gmail.com")
-user3 = User.create!(username:"user",password:"password", email:"user@example.com")
+user1 = User.create!(username: 'abc', password: '12345678', email: 'abc@gmail.com')
+user2 = User.create!(username: 'def', password: '12345678', email: 'bcd@gmail.com')
+user3 = User.create!(username: 'user', password: 'password', email: 'user@example.com')
 
-profile1 = Profile.create!(user: user1)
+profile1 = Profile.create!(user: user1, name: 'profile1')
 feed = UserFeedSubscription.create!(user: user3, profile: profile1)
 
-tag1 = Tag.create!(name: "Funny")
-tag2 = Tag.create!(name: "WTF")
+tag1 = Tag.create!(name: 'Funny')
+tag2 = Tag.create!(name: 'WTF')
