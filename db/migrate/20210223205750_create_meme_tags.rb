b40e4f796cdd7fd3ad5df7e68192696dd4d49047
Bryan Tan <techniux@gmail.com>
2021-03-06 14:50:53 -0800

    Add reset endpoint; improve seed endpoint

--- db/migrate/20210223205750_create_meme_tags.rb
+++ db/migrate/20210223205750_create_meme_tags.rb
@@ -1,8 +1,8 @@
 class CreateMemeTags < ActiveRecord::Migration[6.1]
   def change
     create_table :meme_tags, id: false do |t|
-      t.belongs_to :tag
-      t.belongs_to :meme
+      t.belongs_to :tag, foreign_key: true, null: false
+      t.belongs_to :meme, foreign_key: true, null: false
       t.timestamps
     end
   end
